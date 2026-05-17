0d340caf33d65563324c1411e314891892d1a155
elmira08 <xzrklf@gmail.com>
2026-05-18 01:44:56 +0300

    feat: update hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/layout/header";
-import { HeroSection } from "@/components/sections/hero/hero-section";
+import { Hero } from "@/components/sections/hero-section";
 import { CtaSection } from "@/components/sections/cta-section";
 import { Footer } from "@/components/layout/footer";
 import { BusinessSection } from "@/components/sections/business-section";
@@ -13,7 +13,7 @@ export default function HomePage() {
       <Header />
 
       <main>
-        <HeroSection />
+        <Hero />
         <CtaSection />
         <BusinessSection />
         <RolesSection />
